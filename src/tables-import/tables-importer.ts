b643a60538fbc956ee437c629a8184e2e62c55b6
Alexey Eremin <a.eremin.msu@mail.com>
2018-08-13 17:46:25 +0200

    Generate CSV descriptions of planets

--- src/tables-import/tables-importer.ts
+++ src/tables-import/tables-importer.ts
@@ -3,6 +3,7 @@ import * as PouchDB from 'pouchdb';
 import * as winston from 'winston';
 import * as google from 'googleapis';
 import * as parse from 'csv-parse/lib/sync';
+import * as fs from 'fs';
 
 import { config } from '../config';
 import { saveObject } from '../helpers'
@@ -10,6 +11,33 @@ import { saveObject } from '../helpers'
 import * as loaders from './loaders';
 import { System } from "../interfaces/model";
 import { createEmptyAliceModel } from '../alice-model-converter';
+
+import * as rp from 'request-promise';
+
+interface XenomorphsQrPrintData {
+    planetCode: string;
+    className: string;
+    speciesIndex: number;
+    accountIdCode: string;
+    diseaseCode: string;
+}
+
+async function accountIdCode(id: string): Promise<string> {
+    const r = await rp.get(
+        `http://localhost:8159/encode?type=100&kind=1&validUntil=1680601600&payload=${id}`,
+        { json: true });
+    return r.content;
+}
+
+async function getDiseaseCode(values: number[], power: number): Promise<string> {
+    const seq = [...values, power];
+    const stringifiedSeq = seq.join(',');
+    const r = await rp.get(
+        `http://localhost:8159/encode?type=9&kind=1&validUntil=1680601600&payload=${stringifiedSeq}`,
+        { json: true });
+    return r.content;
+}
+
 
 export class TablesImporter {
 
@@ -29,7 +57,21 @@ export class TablesImporter {
     }
 
 
+
     private readonly numberOfSystems = 7;
+
+    private classNames = [
+        'Одноклеточные',
+        'Растения',
+        'Грибы',
+        'Членистоногие',
+        'Моллюски',
+        'Черви',
+        'Рыбы',
+        'Рептилии',
+        'Птицы',
+        'Млекопитающие',
+    ]
 
     private readonly systemsPresence = [
         [0, 0, 1, 1, 0, 0, 1], // Одноклеточные
@@ -73,10 +115,11 @@ export class TablesImporter {
 
         const data = await loaders.xenomorphsDataLoad(authClient);
         data.values.forEach(async (line, rowIndex: number) => {
-            if (rowIndex > 10) return;
             const planet = line[0];
             if (planet.length == 0)
                 return;
+
+            const xenomorphsQrData: XenomorphsQrPrintData[] = [];
 
             winston.info(`Processing planet ${planet}`);
             for (let i = 0; i < this.systemsPresence.length; ++i) {
@@ -88,6 +131,12 @@ export class TablesImporter {
                 const nucleotide = this.splitCell(nucleotideString);
                 this.assertMatch(nucleotide, systemsMask);
 
+                const diseasePowerString = line[1 + 8 * i + 6];
+                const diseaseValuesString = line[1 + 8 * i + 7];
+                const diseasePower = diseasePowerString == '-' ? 0 : Number(diseasePowerString);
+                const diseaseValues = diseasePowerString == '-' ? [0, 0, 0, 0, 0 ,0 ,0] : this.splitCell(diseaseValuesString);
+                const diseaseCode = await getDiseaseCode(diseaseValues, diseasePower);
+
                 for (let j = 0; j < 5; ++j) {
                     const columnIndex = 1 + 8 * i + j
                     const systemValuesString = line[columnIndex];
@@ -97,9 +146,22 @@ export class TablesImporter {
 
                     const id = '9' + rowIndex.toString().padStart(3, '0') + columnIndex.toString().padStart(2, '0');
 
+                    xenomorphsQrData.push({
+                        accountIdCode: await accountIdCode(id),
+                        className: this.classNames[i],
+                        diseaseCode,
+                        planetCode: planet,
+                        speciesIndex: j + 1,
+                    })
+
                     const systems: System[] = [];
                     for (let s = 0; s < this.numberOfSystems; ++s)
-                        systems.push({lastModified: 0, present: systemsMask[s] == 1, value: systemsValues[s], nucleotide: nucleotide[s]});
+                        systems.push({
+                            lastModified: 0,
+                            present: systemsMask[s] == 1,
+                            value: systemsValues[s],
+                            nucleotide: nucleotide[s]
+                        });
 
                     const model = {
                         ...createEmptyAliceModel(),
@@ -117,9 +179,9 @@ export class TablesImporter {
                     }
                 }
             }
+            if (xenomorphsQrData.length)
+                fs.writeFileSync(`planets/${planet}.csv`, stringify(xenomorphsQrData));
         });
-        //await loaders.testDataSave(authClient);
-        //console.log(JSON.stringify(implants));
     }
 
 
@@ -141,8 +203,11 @@ export class TablesImporter {
 let importer = new TablesImporter();
 
 importer.import().subscribe((result) => {
-    winston.info(`Import finished.`)},
+    winston.info(`Import finished.`)
+},
     (err) => {
         winston.info('Error in import process: ', err);
     }
 );
+
+const stringify = require('csv-stringify/lib/sync')
